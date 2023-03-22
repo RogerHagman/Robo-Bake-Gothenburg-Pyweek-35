f9e5fb1959c85fe1e758d098fcb7c3eb19e2afce
Kim-Caskey-Demaret <128310075+Kim-Caskey-Demaret@users.noreply.github.com>
2023-03-22 17:35:23 +0100

    An almost functional game of Regicide

---
+++ Regicide/engine.py
@@ -0,0 +1,190 @@
+import models
+
+class Engine():
+    draw_deck = None
+    discard_pile = None
+    royal_deck = None
+    player = None
+    hand_limit = 8
+    royal = None
+    won_game = False
+    played_cards = []
+    card_selection = set()
+    game_state = None       #Possible values 'Charge', 'Discard', 'Over'
+
+    def __init__(self) -> None:
+        self.draw_deck = models.Deck('draw')
+        self.discard_pile = models.Deck('discard')
+        self.royal_deck = models.Deck('royal')
+        self.player = models.Player()
+        self.draw_deck.shuffle()
+        while len(self.player.hand)<self.hand_limit:
+            self.player.draw(self.draw_deck)
+        self.royal = models.Royal(self.royal_deck.deal())
+        self.game_state = 'Charge'
+
+    def select_card(self, card):
+        if card in self.card_selection:
+            self.card_selection.remove(card)
+        else:
+            self.card_selection.add(card)
+    
+       
+    def charge(self) ->bool:
+        """
+        Checks if we can call attack
+        """
+        cards = self.card_selection.copy()
+        values = set()
+        for card in cards:
+            values.add(card.get_value())
+        if len(values)>2:       #Cannot play multiple values
+            return False
+        if len(values) == 2:    #Two different values is only allowed with exactly one ace and one other
+            if len(cards)>2:
+                return False
+            aces = len([card for card in cards if card.get_value() == 1])
+            if aces == 1:
+                return True
+            else:
+                return False
+        match len(self.card_selection):
+            case 0:                     #Cannot attack with 0 cards
+                return False
+            case 1:                     #Single card will always work
+                return True
+            case 2:
+                if cards.pop().get_value() >5:  # Double 1s, 2s, 3s, 4s, and 5s permissable
+                    return False
+                else: return True
+            case 3:
+                if cards.pop().get_value() > 3:
+                    return False
+                else: return True
+            case 4:
+                if cards.pop().get_value() > 2:
+                    return False
+                else: return True
+
+    def attack(self):
+        """
+        Attack using current card selection
+        """
+        total_attack = 0
+        played_heart = False
+        played_diamonds = False
+        played_spades = False
+        played_clubs = False
+        for card in self.card_selection:
+            self.player.hand.remove(card)
+            self.played_cards.append(card)
+
+            total_attack += card.get_value()
+            if card.get_suit()!=self.royal.get_suit():
+                match card.get_suit():
+                    case models.Suits.CLUB:
+                        played_clubs = True
+                    case models.Suits.SPADE:
+                        played_spades = True
+                    case models.Suits.HEART:
+                        played_heart = True
+                    case models.Suits.DIAMOND:
+                        played_diamonds = True
+        self.card_selection = set()
+        
+        #Heart effect
+        if played_heart:
+            self.discard_pile.shuffle()
+            self.draw_deck.refresh(total_attack, self.discard_pile)
+
+        #Diamond effect
+        if played_diamonds:
+            draw_cards = min(total_attack, self.hand_limit-len(self.player.hand))
+            for n in range(draw_cards):
+                try:
+                    self.player.draw(self.draw_deck)
+                except:
+                    pass
+
+        #Spade effect
+        if played_spades:
+            self.royal.block(total_attack)
+
+        #Clubs effect        
+        if played_clubs:
+            total_attack = total_attack*2
+    
+        #Player attacks
+        self.royal.attack(total_attack)
+
+        #Cleanup when royal defeated
+        health_check = self.royal.get_health()
+        if health_check < 1:
+            defeated = models.Card(self.royal.get_suit(), self.royal.get_value())
+            if health_check == 0:
+                self.draw_deck.add_to_top(defeated)
+            else:
+                self.discard_pile.add_to_top(defeated)
+            self.royal = None
+            for card in self.played_cards:
+                self.discard_pile.add_to_top(card)
+            self.played_cards = []
+            try:
+                self.royal = models.Royal(self.royal_deck.deal())
+            except Exception:
+                self.won_game = True
+                self.game_state = 'Over'
+        else:
+            if self.royal.get_attack()>0:
+                if len(self.player.hand)>0:
+                    self.game_state = 'Discard'
+                else:
+                    self.game_state = 'Over'
+    
+    def discard(self):
+        """
+        Check if selected cards are enough to discard
+        """
+        #Player defends against royal attack value by discarding cards
+        discard_amount = self.royal.defend()
+        value = 0
+        for card in self.card_selection:
+            value += card.get_value()
+        if value >= discard_amount:
+            return True
+        else: return False
+    
+    def defend(self):
+        """
+        Discard card selection
+        """
+        for card in self.card_selection:
+            self.player.hand.remove(card)
+            self.discard_pile.add_to_top(card)
+        self.game_state = 'Charge'
+        self.card_selection = set()
+            
+    def get_selected(self):
+        return self.card_selection
+    
+    def get_hand(self):
+        return self.player.hand
+    
+    def get_played(self):
+        return self.played_cards
+    
+    def get_state(self):
+        return self.game_state
+    
+    def get_royal_immunity(self):
+
+        #power_text = f"Hearts: refresh draw pile\nDiamonds: draw cards\nSpades: block attack.\nClubs: double your attack.\n"
+
+        if self.royal.get_suit() == models.Suits.CLUB:
+            return f"The {self.royal.get_rank()} of Clubs immune to the attack doubling power of clubs"
+        elif self.royal.get_suit() == models.Suits.SPADE:
+            return f"The {self.royal.get_rank()} of Spades immune to the blocking power of spades"
+        elif self.royal.get_suit() == models.Suits.HEART:
+            return f"The {self.royal.get_rank()} of Hearts stops the refresh power of hearts"
+        elif self.royal.get_suit() == models.Suits.DIAMOND:
+            return f"The {self.royal.get_rank()} of Diamonds stops the redraw power of diamonds"
---
+++ Regicide/models.py
@@ -0,0 +1,139 @@
+from enum import Enum
+import pygame
+import random
+
+class Suits(Enum):
+    CLUB = 0
+    SPADE = 1
+    HEART = 2
+    DIAMOND = 3
+
+class Card:
+    suit = None
+    value = None
+    image = None
+
+    def __init__(self, suit, value, size='small'):
+        self.suit = suit
+        self.value = value
+        self.image = pygame.image.load('images/' + self.suit.name + '-' + str(self.value) + '.svg')
+        if size == 'small':
+            self.image = pygame.transform.scale(self.image, (int(238*0.4), int(332*0.4)))
+        else:
+            self.image = pygame.transform.scale(self.image, (int(238*0.8), int(332*0.8)))
+    def get_value(self):
+        return self.value
+
+    def get_suit(self):
+        return self.suit
+    
+class Deck:
+    cards = None
+    def __init__(self, name) -> None:
+        self.cards = []
+        if name == "draw":
+            for suit in Suits:
+                for value in range(1,11):
+                    self.cards.append(Card(suit, value))
+        elif name == "royal":
+            jacks = []
+            queens = []
+            kings = []
+            for suit in Suits:
+                kings.append(Card(suit, 13, 'large'))
+            random.shuffle(kings)
+            self.cards.extend(kings)
+            for suit in Suits:
+                queens.append(Card(suit, 12, 'large'))
+            random.shuffle(queens)
+            self.cards.extend(queens)
+            for suit in Suits:
+                jacks.append(Card(suit, 11, 'large'))
+            random.shuffle(jacks)
+            self.cards.extend(jacks)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        return self.cards.pop()
+
+    def length(self):
+        return len(self.cards)
+  
+    def refresh(self, cards:int, pile):
+        self.cards.reverse()
+        for n in range(cards):
+            try: 
+                self.cards.append(pile.deal())
+            except:
+               pass
+        self.cards.reverse()
+    
+    def add_to_top(self, card:Card):
+       self.cards.append(card)
+
+class Player:
+    hand = None
+
+    def __init__(self):
+        self.hand = []
+
+    def draw(self, deck):
+        self.hand.append(deck.deal())
+
+    def play(self) -> Card:
+        return self.hand.pop(0)
+
+class Royal:
+    health = None
+    attack_value = None
+    suit = None
+    card = None
+    rank = None
+
+    def __init__(self, card:Card) -> None:
+        self.card = card
+        self.suit = card.get_suit()
+        if card.get_value() == 11:
+            self.health = 20
+            self.attack_value = 10
+            self.rank = 'Jack'
+        elif card.get_value() == 12:
+            self.health = 30
+            self.attack_value = 15
+            self.rank = 'Queen'
+        else:
+            self.health = 40
+            self.attack_value = 20
+            self.rank = 'King'
+
+    def attack(self, damage:int):
+        "Player attacking royal"
+        self.health -= damage
+    
+    def block(self, block:int):
+       "Royal being blocked"
+       self.attack_value -= block
+
+    def defend(self):
+        "Royal attacking player"
+        return self.attack_value
+    
+    def get_health(self):
+       return self.health
+    
+    def get_suit(self):
+       return self.suit
+    
+    def get_rank(self):
+        return self.rank
+    
+    def get_card(self):
+       return self.card
+    
+    def get_attack(self):
+        return self.attack_value
+    
+    def get_value(self):
+        return self.card.get_value()
