8d8555276f90f96da49bda4148984eff325f6495
Kim-Caskey-Demaret <kim.demaret@gmail.com>
2023-03-24 11:23:19 +0100

    Collision management

--- Animation/feedthegoblins.py
+++ Animation/feedthegoblins.py
@@ -8,64 +8,12 @@ We will start with one apple on the ground and one goblin.
 
 import pygame
 
-import goblinclasses
+from goblinclasses import FeedTheGoblins
 
 pygame.init()
 
-win = pygame.display.set_mode((1002,480))
-pygame.display.set_caption("Feed the Goblins")
-bg = pygame.image.load('bg.jpg')
+game = FeedTheGoblins('bg.jpg')
 
-sb = goblinclasses.StatusArea(size=(150,480), axis=1, font=pygame.font.SysFont('Arial', (20)))
-sb.update(('Health', 5))
-sb.update(('Apples', 0))
-sb.update(('Wins', 'YES'))
+game.run_game()
 
-apple = goblinclasses.Still((20,400), 'apple.png', (25,25))
-
-hero = goblinclasses.Walker((150,350),'standing.png')
-
-goblin = goblinclasses.Walker((400, 350), 'L1E.png', 'E')
-
-clock = pygame.time.Clock()
-
-def redrawGameWindow(window, walkers, stills):
-    """
-    Draws window, and all sprites
-    """
-    
-    window.blit(bg, (0,0))
-    window.blit(sb.draw(), (852,0))
-
-    for sprite in walkers:
-        step, x, y = sprite.walk()
-        window.blit(step, (x,y))
-
-    for sprite in stills:
-        img, x, y = sprite.draw()
-        window.blit(img, (x,y))
-
-    pygame.display.update() 
-
-run = True
-
-while run:
-    clock.tick(27)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        hero.walkLeft()
-    elif keys[pygame.K_RIGHT]:
-        hero.walkRight()    
-    else:
-        hero.stand()
-    if keys[pygame.K_SPACE]:
-        hero.jump()
-
-    redrawGameWindow(win, [hero, goblin], [apple]) 
-    
 pygame.quit()
--- Animation/goblinclasses.py
+++ Animation/goblinclasses.py
@@ -1,35 +1,35 @@
-from pygame import sprite, image, transform, surface
+import pygame
 
-class Walker(sprite.Sprite):
+class Walker(pygame.sprite.Sprite):
     """
     Sprite that walks
     """
-    walkL = []   # List of images to animate walking left
-    walkR = []
+    def __init__(self, *groups, pos, img, imgcode='', steps=9, width=40, height=60, vel=5, lim=852, timer=27) -> None:
+        super().__init__(*groups)
+        self.walkL = []   # List of images to animate walking left
+        self.walkR = []
 
-    isJump = False
-    jumpCount = 10
+        self.isJump = False
+        self.jumpCount = 10
 
-    left = False    # Currently facing/walking left
-    right = False
-    walkCount = 0
-
-    def __init__(self, pos, img, imgcode='', steps=9, width=40, height=60, vel=5, lim=852, timer=27) -> None:
-        super().__init__(   )
+        self.left = False    # Currently facing/walking left
+        self.right = False
+        self.walkCount = 0
 
         # Starting position, tuple of coordinates
         self.x = pos[0]
         self.y = pos[1]
+        self.rect = pygame.Rect(self.x, self.y, width, height)
         
         #Load images
         #Images for walking animation should be named "L1"+imgcode / "R1"+imgcode
-        self.img = image.load(img)
+        self.img = pygame.image.load(img)
         self.steps = steps
         for step in range(steps):
             l = 'L' + str(step+1) + imgcode + '.png'
-            self.walkL.append(image.load(l))
+            self.walkL.append(pygame.image.load(l))
             r = 'R' + str(step+1) + imgcode + '.png'
-            self.walkR.append(image.load(r))
+            self.walkR.append(pygame.image.load(r))
         
         #Controls the walking
         self.width = width
@@ -81,6 +81,9 @@ class Walker(sprite.Sprite):
         else:
             step = self.img
         
+        #Rect is what is needed for sprite's built in collision methods, important to update!
+        self.rect.update(self.x, self.y, self.width, self.height)
+        
         return step, self.x, self.y
     
     def jump(self):
@@ -89,14 +92,50 @@ class Walker(sprite.Sprite):
             self.right = False
             self.left = False
             self.walkCount = 0
-    
-class Still(sprite.Sprite):
 
-    def __init__(self, pos, img, size) -> None:
-        super().__init__()
+class Hero(Walker):
+    health = 5
+    apples = 0
+    goblins_fed = 0
+
+    def __init__(self, pos, img) -> None:
+        super().__init__(pos=pos, img=img)
+
+    def pick_up_apple(self):
+        self.apples+=1
+
+    def be_eaten(self):
+        self.health =-1
+        self.health = max(0, self.health)
+    
+    def walk(self):
+        return super().walk()
+    
+    def walkLeft(self):
+        super().walkLeft()
+
+    def walkRight(self):
+        super().walkRight()
+
+    def jump(self):
+        super().jump()
+    
+    def get_health(self):
+        return ('Health', self.health)
+    
+    def get_apples(self):
+        return ('Apples', self.apples)
+        
+    def get_goblins(self):
+        return ('Goblins Fed', self.goblins_fed)
+
+class Still(pygame.sprite.Sprite):
+    def __init__(self, *groups, pos, img, size) -> None:
+        super().__init__(*groups)
         self.x = pos[0]
         self.y = pos[1]
-        self.img = transform.scale(image.load(img), (size))
+        self.img = pygame.transform.scale(pygame.image.load(img), (size))
+        self.rect = pygame.Rect(self.x, self.y, size[0], size[1])
     
     def draw(self):
         return self.img, self.x, self.y
@@ -108,7 +147,7 @@ class StatusArea():
     # First element a unique string
 
     def __init__(self, size, axis, font, color = (255,255,255)) -> None:
-        self.area = surface.Surface((size))
+        self.area = pygame.surface.Surface((size))
         self.color = color
         self.size = size
         if axis == 0:
@@ -148,3 +187,89 @@ class StatusArea():
 
         return self.area
         
+class FeedTheGoblins():
+    """
+    Main game class
+    """
+    def __init__(self, bg) -> None:
+        self.win = pygame.display.set_mode((1002,480))
+        pygame.display.set_caption("Feed the Goblins")
+        self.bg = pygame.image.load(bg)
+
+        self.sb = StatusArea(size=(150,480), axis=1, font=pygame.font.SysFont('Arial', (20)))
+        self.apples = pygame.sprite.Group()
+        self.goblins = pygame.sprite.Group()
+
+        apple = Still(pos=(20,400), img='apple.png', size=(25,25))
+        self.apples.add(apple)
+
+        goblin = Walker(pos=(400, 350), img='L1E.png', imgcode='E')
+        self.goblins.add(goblin)
+
+        self.hero = Hero(pos=(150,350),img='standing.png')
+
+        self.sb.update(self.hero.get_health())
+        self.sb.update(self.hero.get_apples())
+
+        self.clock = pygame.time.Clock()
+
+
+    def redrawGameWindow(self):
+        """
+        Draws window, and all sprites
+        """
+        self.win.blit(self.bg, (0,0))
+        self.win.blit(self.sb.draw(), (852,0))
+
+        step, x, y = self.hero.walk()
+        self.win.blit(step, (x,y))
+
+        for sprite in self.goblins:
+            step, x, y = sprite.walk()
+            self.win.blit(step, (x,y))
+
+        for sprite in self.apples:
+            img, x, y = sprite.draw()
+            self.win.blit(img, (x,y))
+
+        pygame.display.update() 
+    
+    def run_game(self):
+
+        run = True
+
+        while run:
+            self.clock.tick(27)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.hero.walkLeft()
+            elif keys[pygame.K_RIGHT]:
+                self.hero.walkRight()    
+            else:
+                self.hero.stand()
+            if keys[pygame.K_SPACE]:
+                self.hero.jump()
+
+            #Collisions
+            apple = pygame.sprite.spritecollideany(self.hero, self.apples)
+            if apple != None:
+                self.hero.pick_up_apple()
+                self.apples.remove(apple)
+                self.sb.update(self.hero.get_apples())
+            
+            goblin = pygame.sprite.spritecollideany(self.hero, self.goblins)
+            if goblin !=None:
+                if self.hero.get_apples()[1]>0:
+                    pass
+                else:
+                    self.hero.be_eaten()
+                    self.sb.update(self.hero.get_health())
+
+
+            self.redrawGameWindow() 
+
