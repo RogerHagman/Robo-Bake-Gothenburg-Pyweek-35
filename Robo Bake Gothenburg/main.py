24f2c2a486eeb9f6a732452161cfa481ee2f8880
RogerHagman <rogerhagman84@gmail.com>
2023-03-26 12:24:13 +0200

    class structure

--- Robo Bake Gothenburg/main.py
+++ Robo Bake Gothenburg/main.py
@@ -1,1 +1,27 @@
 import pygame
+"""Robo Bake Gothenburg Unnamed Game"""
+
+class Game():
+    """ """
+    pass
+class Level():
+    """ """
+    pass
+class Menu():
+    """ """
+    pass
+class GameObject(pygame.sprite.Sprite):
+    """ """
+    pass
+class Player(GameObject):
+    """ """
+    pass
+class Hud(GameObject):
+    """ """
+    pass
+class Map():
+    """ """
+    pass
+
+class Dialogues():
+    """ """
