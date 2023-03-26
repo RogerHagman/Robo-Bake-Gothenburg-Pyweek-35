6d203c4d16ebc78b3aba7b6d41192ad6800068ff
Kim-Caskey-Demaret <kim.demaret@gmail.com>
2023-03-26 14:42:45 +0200

    Black and white menu

--- Robo Bake Gothenburg/main.py
+++ Robo Bake Gothenburg/main.py
@@ -4,12 +4,102 @@ import pygame
 class Game():
     """ """
     pass
+
 class Level():
-    """ """
-    pass
-class Menu():
-    """ """
-    pass
+    """ 
+    Base class for level design.
+    
+    Because the Game class has control over our pygame.display,
+    any Level class needs a way to return its Surface to the Game.
+    """
+    def __init__(self, width:int, height:int) -> None:
+        """
+        Initialize surface, game objects and states
+        """
+        self.run = True
+        self.surface = pygame.surface.Surface((width, height))
+
+    def render_level(self) -> pygame.surface.Surface:
+        """
+        Blit game objects onto level surface and returns surface
+        """
+        #Blit
+        #Blit
+        return self.surface
+
+    def run_level(self) -> bool:
+        """
+        Handles events and returns bool for Game to know the level is still running
+        """
+        for event in pygame.event.get():        # All run_levels should start with this.
+            if event.type == pygame.QUIT:       # I do not know why.
+               self.run = False
+        #pygame events
+        #collisions
+        return self.run
+        
+class TelephoneRoom(Level):
+    """
+    An office space with telephones
+    """
+
+    def __init__(self, width: int, height: int) -> None:
+        super().__init__(width, height)
+        new_map = Map(width, height)            # Does Map need more parameters in init?
+        self.map = Map.draw()                   # What's this method really called? Are we getting a surface or array?
+
+
+
+    def render_level(self) -> pygame.surface.Surface:
+        return super().render_level()
+    
+    def run_level(self) -> bool:
+        return super().run_level()
+
+
+class Menu(Level):
+    """
+    Start Menu
+    """
+    def __init__(self, width: int, height: int) -> None:
+        super().__init__(width, height)
+
+        self.font = pygame.font.SysFont('arial', 40)
+        self.surface.fill((0,0,0))
+        self.title = self.font.render('Robo Bake Gothenburg', True, (255, 255, 255))
+        start_button = self.font.render('Start', True, (255, 255, 255))
+        quit_button = self.font.render('Quit', True, (255, 255, 255))
+        pie_button = self.font.render('Pie recipes', True, (255, 255, 255))
+
+        self.surface.blit(self.title, (width/2 - self.title.get_width()/2, 50))
+        
+        self.start_button = self.surface.blit(start_button, (width/2 - start_button.get_width()/2, 200))
+        self.quit_button = self.surface.blit(quit_button, (width/2 - quit_button.get_width()/2, 250 ))
+        self.pie_button = self.surface.blit(pie_button, (width/2 - pie_button.get_width()/2, 300))
+
+
+    def run_level(self) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               self.run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                click = event.pos
+                if self.start_button.collidepoint(click):
+                    print("There's no level to start yet")
+                    self.run = False
+                elif self.quit_button.collidepoint(click):
+                    print("This quit button works as intended")
+                    self.run = False
+                elif self.pie_button.collidepoint(click):
+                    print("Mmmm pie")
+                    self.run = False
+        return self.run
+    
+    def render_level(self) -> pygame.surface.Surface:
+
+        return self.surface
+
+
 class GameObject(pygame.sprite.Sprite):
     """ """
     pass
---
+++ Robo Bake Gothenburg/test_menu_appearance.py
@@ -0,0 +1,19 @@
+from main import Menu
+
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((500, 500))
+clock.tick(10)
+start_menu = Menu(500, 500)
+
+while start_menu.run_level():
+    screen.blit(start_menu.render_level(), (0,0))
+    clock.tick(10)
+    pygame.display.update()
+
+
+pygame.quit()
