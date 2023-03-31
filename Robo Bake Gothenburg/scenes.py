13c953cc5fd9ec742ec1ab6db9860d80ce42cc58
Kim-Caskey-Demaret <kim.demaret@gmail.com>
2023-03-31 09:11:55 +0200

    Split into scenes.py, level_components.py

--- Robo Bake Gothenburg/scenes.py
+++ Robo Bake Gothenburg/scenes.py
@@ -2,6 +2,7 @@ import pygame
 import re
 from settings import *
 from sprites import *
+from level_components import *
 
 class Level():
     """ 
@@ -138,119 +139,14 @@ class Menu(Level):
     def started(self):
         return self.start
 
-class Map(): 
-
-    def __init__(self,lvl:str, map_size):
-        """_summary_
-        Args:
-            lvl (str): specifies which map to load, 
-            map_size is set by screen height.
-        """
-        self.tile_size = map_size//20
-        self.wall_list = []
-        self.door_list = []
-        self.player = None
-        self.enemy_list = []
-        self.pie_list = []
-        self.clutter_list = []
-        self.distractions_list = []
-        self.player_pos = (0,0)
-        
-        # Load images
-        # Plant and desks are not the same height and width
-        # If in doubt, use 'keep aspect ratio'
-        wall_img = pygame.transform.scale(pygame.image.load(WALL_IMG),(self.tile_size,self.tile_size))#1
-        door_img = pygame.transform.scale(pygame.image.load(DOOR_IMG),(self.tile_size,self.tile_size))#3
-        enemy1_img = pygame.transform.scale(pygame.image.load(ENEMY1_IMG),(self.tile_size,self.tile_size))#4
-        enemy2_img = pygame.transform.scale(pygame.image.load(ENEMY2_IMG),(self.tile_size,self.tile_size))#5
-        pie_img = pygame.transform.scale(pygame.image.load(PIE_IMG),(self.tile_size,self.tile_size))#6
-        plant_img = self.keep_aspect_ratio(pygame.image.load(PLANT_IMG))
-        phone_img = pygame.transform.scale(pygame.image.load(PHONE_IMG),(self.tile_size,self.tile_size))#8
-        desk_img = self.keep_aspect_ratio(pygame.image.load(DESK_IMG))#9
-
-        with open(lvl) as file:
-            map = file.read().splitlines()
-        
-        row_count = 0
-        for row in map:
-            col_count = 0
-            for tile in row:
-                if tile != '.':
-                    tile = int(tile)
-                if tile == 1:                           # 1 = wall 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                    wall = Wall(x=x, y=y, figure=wall_img)
-                    self.wall_list.append(wall)
-                if tile == 2:                           # 2 = player 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                    self.player_pos = (x,y)
-                if tile == 3:                           # 3 = door 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                    door = Door(x=x, y=y, figure=door_img)
-                    self.door_list.append(door)
-                if tile == 4:                           # 4 = enemy1 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                    enemy1 = Enemy(x=x, y=y, figure=enemy1_img)
-                    self.enemy_list.append(enemy1)
-                if tile == 5:                           # 5 = enemy2 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                    enemy2 = Enemy(x=x, y=y, figure=enemy2_img)
-                    self.enemy_list.append(enemy2)
-                if tile == 6:                           # 6 = pie 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                #    pie = Pie(x=x, y=y, figure=pie_img)
-                #    self.pie_list.append(pie)
-                if tile == 7:                           # 7 = plant 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                #    plant = Clutter(x=x, y=y, figure=img)
-                #    self.clutter_list.append(plant)
-                if tile == 8:                           # 8 = phone 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                #    phone = Distraction(x=x, y=y, figure=img)
-                #    self.distractions_list.append(phone)
-                if tile == 9:                           # 9 = desk 
-                    x = col_count * self.tile_size
-                    y = row_count * self.tile_size
-                #    phone = Distraction(x=x, y=y, figure=img)
-                #    self.distractions_list.append(phone)
-                col_count += 1
-                
-            row_count += 1
-    # getters for object lists
-    def get_player_pos(self):
-        return self.player_pos
-    def get_walls(self):
-        return self.wall_list
-    def get_doors(self):
-        return self.door_list
-    def get_enemies(self):
-        return self.enemy_list
-    def get_pies(self):
-        return self.pie_list
-    def get_clutter(self):
-        return self.clutter_list
-    def get_distractions(self):
-        return self.distractions_list
-
-    def keep_aspect_ratio(self, img):
-        """
-        Returns image scaled to tile size, maintaining aspect ratio
-        NP: the pygame.transform.scale_by() function is experimental
-        """
-        biggest_side = max(img.get_width(), img.get_height())
-        return pygame.transform.scale_by(img, self.tile_size/biggest_side)
 
 class Dialogue(Level):
     """
-    Loads a text file and creates a dialogue scene
+    Loads a text file and creates a dialogue scene.
+
+    Each "turn", with one statement from PRINTO3000,
+    and multiple responses for the player to choose,
+    is a DialogueTurn object contained in diadict.
     """
 
     def __init__(self, width, height, text_file:str) -> None:
@@ -271,7 +167,7 @@ class Dialogue(Level):
         for section in found:
             id = int(re.match(r'([0-9]*)', section).group())
             printer_says = re.match(r'(?:[0-9]*)(.+)', section).groups(1)[0]
-            turn = DialogueOptions(id, printer_says)
+            turn = DialogueTurn(id, printer_says)
             options = re.findall(r'(?:\*)(.+)', section)
             for opt in options:
                 # Pattern splits in to list like:
@@ -373,19 +269,3 @@ class Dialogue(Level):
             # remove the text we just blitted
             text = text[i:]
         return text, y-rect.top
-
-class DialogueOptions():
-
-    def __init__(self, id, p) -> None:
-        self.id = id
-        self.printer_says = p   # One string
-        self.options = []       # List of tuples (string: what the player says, int: the turn this choice will lead to)
-    
-    def add_option(self, option):
-        self.options.append(option)
-    
-    def get_printer(self):
-        return self.printer_says
-    
-    def get_options(self):
-        return self.options
