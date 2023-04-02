7f39ead03e8599133fad3137a6200b13d7e13207
RogerHagman <rogerhagman84@gmail.com>
2023-04-02 13:22:26 +0200

    Executable

--- Robo Bake Gothenburg/Printo-the-sweet-escape/settings.py
+++ Robo Bake Gothenburg/Printo-the-sweet-escape/settings.py
@@ -18,7 +18,8 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 SCREEN_SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
 TITLE = 'Robo Bake Gothenburg'
-ASSETS_PATH = os.path.join(sys.path[0], 'Assets')
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+# SCRIPT_PATH = os.path.join(sys.path[0], 'Assets')
 
 # Grids and Tiles
 TILESIZE = SCREEN_HEIGHT // 20
@@ -26,8 +27,8 @@ GRIDWIDTH = SCREEN_WIDTH / TILESIZE
 GRIDHEIGHT = SCREEN_HEIGHT / TILESIZE
 
 # Fonts
-SCENE_FONT = os.path.join(ASSETS_PATH, 'Roboto-Light.ttf')
-SCENE_FONT_2 = os.path.join(ASSETS_PATH, 'Roboto-Regular.ttf')
+SCENE_FONT = os.path.join(SCRIPT_PATH,'Assets', 'Roboto-Light.ttf')
+SCENE_FONT_2 = os.path.join(SCRIPT_PATH,'Assets', 'Roboto-Regular.ttf')
 SCENE_FONT_TINY = TILESIZE//4
 SCENE_FONT_SMALL = TILESIZE//2
 SCENE_FONT_MEDIUM = TILESIZE//2 + TILESIZE//4 + TILESIZE//8
@@ -44,39 +45,39 @@ ENEMY_SPEED = 2
 ENEMY_DISTRACT_TIME = 5
 
 # Image paths
-BG_IMG = os.path.join(ASSETS_PATH,"bg.png")
-WALL_IMG = os.path.join(ASSETS_PATH,"wall.png")
-PLAYER_IMG = os.path.join(ASSETS_PATH,"player.png")
-DOOR_IMG = os.path.join(ASSETS_PATH,"door.png")
-ENEMY1_IMG = os.path.join(ASSETS_PATH,"enemy1.png")
-ENEMY2_IMG = os.path.join(ASSETS_PATH,"enemy2.png")
-PIE_IMG = os.path.join(ASSETS_PATH,"pie.png")
-PLANT_IMG = os.path.join(ASSETS_PATH,"plant.png")
-PHONE_IMG = os.path.join(ASSETS_PATH,"phone.png")
-DESK_IMG = os.path.join(ASSETS_PATH,"desk1.png")
-TABLE_IMG = os.path.join(ASSETS_PATH,"lonely_table.png")
-LONE_PIE_IMG = os.path.join(ASSETS_PATH,"lonely_pie.png")
-HEART_IMG = os.path.join(ASSETS_PATH,"heart.png")
-FOG_IMG = os.path.join(ASSETS_PATH,"cutout1.png")
-DARK_IMG = os.path.join(ASSETS_PATH,"cutout2.png")
+BG_IMG = os.path.join(SCRIPT_PATH,'Assets',"bg.png")
+WALL_IMG = os.path.join(SCRIPT_PATH,'Assets',"wall.png")
+PLAYER_IMG = os.path.join(SCRIPT_PATH,'Assets',"player.png")
+DOOR_IMG = os.path.join(SCRIPT_PATH,'Assets',"door.png")
+ENEMY1_IMG = os.path.join(SCRIPT_PATH,'Assets',"enemy1.png")
+ENEMY2_IMG = os.path.join(SCRIPT_PATH,'Assets',"enemy2.png")
+PIE_IMG = os.path.join(SCRIPT_PATH,'Assets',"pie.png")
+PLANT_IMG = os.path.join(SCRIPT_PATH,'Assets',"plant.png")
+PHONE_IMG = os.path.join(SCRIPT_PATH,'Assets',"phone.png")
+DESK_IMG = os.path.join(SCRIPT_PATH,'Assets',"desk1.png")
+TABLE_IMG = os.path.join(SCRIPT_PATH,'Assets',"lonely_table.png")
+LONE_PIE_IMG = os.path.join(SCRIPT_PATH,'Assets',"lonely_pie.png")
+HEART_IMG = os.path.join(SCRIPT_PATH,'Assets',"heart.png")
+FOG_IMG = os.path.join(SCRIPT_PATH,'Assets',"cutout1.png")
+DARK_IMG = os.path.join(SCRIPT_PATH,'Assets',"cutout2.png")
 
 # Sound
-PLAYER_SOUND = os.path.join(ASSETS_PATH,"receipt-printer-01-43872.mp3")
-PHONE_SOUND = os.path.join(ASSETS_PATH,"synth-telephone-ring-001-8434.mp3")
-ENEMY_TALK = os.path.join(ASSETS_PATH,"muffled-talking-6161.mp3")
-AMBIANCE_SOUND = os.path.join(ASSETS_PATH, 'menschenmenge-142716.mp3')
+PLAYER_SOUND = os.path.join(SCRIPT_PATH,'Assets',"receipt-printer-01-43872.mp3")
+PHONE_SOUND = os.path.join(SCRIPT_PATH,'Assets',"synth-telephone-ring-001-8434.mp3")
+ENEMY_TALK = os.path.join(SCRIPT_PATH,'Assets',"muffled-talking-6161.mp3")
+AMBIANCE_SOUND = os.path.join(SCRIPT_PATH,'Assets', 'menschenmenge-142716.mp3')
 # Dialogue texts
-START_DIALOGUE = os.path.join(ASSETS_PATH,"start_text.txt")
-DIALOGUE_TWO = os.path.join(ASSETS_PATH,"dialogue_two.txt")
-DIALOGUE_THREE = os.path.join(ASSETS_PATH,"dialogue_three.txt")
-FINAL_DIALOGUE = os.path.join(ASSETS_PATH,"final_text.txt")
+START_DIALOGUE = os.path.join(SCRIPT_PATH,'Assets',"start_text.txt")
+DIALOGUE_TWO = os.path.join(SCRIPT_PATH,'Assets',"dialogue_two.txt")
+DIALOGUE_THREE = os.path.join(SCRIPT_PATH,'Assets',"dialogue_three.txt")
+FINAL_DIALOGUE = os.path.join(SCRIPT_PATH,'Assets',"final_text.txt")
 
 # Maps
-MAP_ONE = os.path.join(ASSETS_PATH,"map1.txt")
-MAP_TWO = os.path.join(ASSETS_PATH,"map2.txt")
-MAP_THREE = os.path.join(ASSETS_PATH,"map3.txt")
+MAP_ONE = os.path.join(SCRIPT_PATH,'Assets',"map1.txt")
+MAP_TWO = os.path.join(SCRIPT_PATH,'Assets',"map2.txt")
+MAP_THREE = os.path.join(SCRIPT_PATH,'Assets',"map3.txt")
 
 # Credits
-CREDITS_TEXT = os.path.join(ASSETS_PATH,"credits_text.txt")
-RBG_TEXT = os.path.join(ASSETS_PATH,"robo_bakers.txt")
-CREDITS_IMG = os.path.join(ASSETS_PATH,"creditspic.jpg")
+CREDITS_TEXT = os.path.join(SCRIPT_PATH,'Assets',"credits_text.txt")
+RBG_TEXT = os.path.join(SCRIPT_PATH,'Assets',"robo_bakers.txt")
+CREDITS_IMG = os.path.join(SCRIPT_PATH,'Assets',"creditspic.jpg")
